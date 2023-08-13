0030fe1bc51b0e7fbe91069714173a069963119a
Hellblazer <hellblazer@me.com>
2023-08-13 07:18:55 -0700

    it works

---
+++ FXyz-Core/src/test/java/org/fxyz3d/Sample.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2008-2023 Hal Hildebrand. All rights reserved.
+ * 
+ * This file is part of the Prime Mover Event Driven Simulation Framework.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fxyz3d;
+
+import org.fxyz3d.shapes.primitives.SpringMesh;
+import org.fxyz3d.utils.CameraTransformer;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.CullFace;
+import javafx.stage.Stage;
+
+/**
+ * 
+ */
+public class Sample extends Application {
+    public static class Launcher {
+
+        public static void main(String[] argv) {
+            Sample.main(argv);
+        }
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setNearClip(0.1);
+        camera.setFarClip(10000.0);
+        camera.setTranslateX(10);
+        camera.setTranslateZ(-100);
+        camera.setFieldOfView(20);
+
+        CameraTransformer cameraTransform = new CameraTransformer();
+        cameraTransform.getChildren().add(camera);
+        cameraTransform.ry.setAngle(-30.0);
+        cameraTransform.rx.setAngle(-15.0);
+
+        SpringMesh spring = new SpringMesh(10, 2, 2, 8 * 2 * Math.PI, 200, 100, 0, 0);
+        spring.setCullFace(CullFace.NONE);
+        spring.setTextureModeVertices3D(1530, p -> p.f);
+
+        Group group = new Group(cameraTransform, spring);
+
+        Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
+        scene.setFill(Color.BISQUE);
+        scene.setCamera(camera);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("FXyz3D Sample");
+        primaryStage.show();
+    }
+
+}
